a942b2f30f5054fcb05e4bca5d42c7d369ac4b99
elgondor <gondorrexxar@hotmail.com>
2021-05-16 02:26:46 -0400

    Seted models, serializers and seeders

--- api/models.py
+++ api/models.py
@@ -7,23 +7,16 @@ class Client(models.Model):
         REGULAR = 2
         GOOD = 3
 
+    name = models.CharField(max_length=200, null=True)
     SBS_debt = models.IntegerField()
-    client_score = models.IntegerField(choices = Score.choices)
-    sentinel_score = models.IntegerField()
+    sentinel_score = models.IntegerField(choices = Score.choices)
 
     @staticmethod
-    def name(self):
-        return 'henry'
+    def ai_algorithm():
+        return random.randrange(1, 10)
 
 
 class PersonalCredit(models.Model):
     client = models.ForeignKey(Client, on_delete=models.CASCADE)
     amount = models.IntegerField()
     approvement = models.BooleanField(default=False)
-
-    @staticmethod
-    def ai_algorithm():
-        return random.randrange(1, 10)
-    
-    
-
--- api/serializers.py
+++ api/serializers.py
@@ -2,18 +2,16 @@ from rest_framework import serializers
 from .models import *
 
 class PersonalCreditSerializer(serializers.ModelSerializer):
-    # client_name = serializers.CharField(source='client.name')
+    client_name = serializers.CharField(source='client.name')
     class Meta:
         model = PersonalCredit
-        fields = ('id', 'amount', 'approvement', 'client', 'ai_algorithm',)
+        fields = ('id', 'amount', 'approvement', 'client', 'client_name')
 
 
 class ClientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Client
-        fields = ('id', 'SBS_debt', 'client_score', 'sentinel_score')
-
-
+        fields = ('id', 'name', 'SBS_debt', 'sentinel_score', 'ai_algorithm',)
 
 class PersonalCreditUpdateSerializer(serializers.ModelSerializer):
     class Meta:
